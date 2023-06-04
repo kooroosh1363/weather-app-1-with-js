dae5a1cd8f53ffcfb87daf4e291ae5cbfd352fd2
kooroosh1363 <peymanraad1363@gmail.com>
2023-06-04 16:26:34 +0400

    create var for each element

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+var cityInput = document.getElementById("cityInput");
+var addInput = document.getElementById("add");
+var cityOutPut = document.getElementById("cityoutput");
+var descOutPut = document.getElementById("description");
+var tempOutPut = document.getElementById("temperature");
+var windOutPut = document.getElementById("wind");
