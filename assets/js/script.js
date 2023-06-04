d29c69ca7fb4077194e3fb4b3e5d5ff5cb5f5ea4
kooroosh1363 <peymanraad1363@gmail.com>
2023-06-04 16:29:32 +0400

    create const for apikey

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,5 @@ var cityOutPut = document.getElementById("cityoutput");
 var descOutPut = document.getElementById("description");
 var tempOutPut = document.getElementById("temperature");
 var windOutPut = document.getElementById("wind");
+
+const = apiKey = "bcc9d715a88df781d66d51a02062e0d2";
