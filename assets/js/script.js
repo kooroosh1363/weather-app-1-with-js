3f954dd2488583e9daebe34fce9ce1344e9fd699
kooroosh1363 <peymanraad1363@gmail.com>
2023-06-04 17:19:04 +0400

    fetch api and json response

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,4 +5,18 @@ var descOutPut = document.getElementById("description");
 var tempOutPut = document.getElementById("temperature");
 var windOutPut = document.getElementById("wind");
 
-const = apiKey = "bcc9d715a88df781d66d51a02062e0d2";
+const apiKey = "bcc9d715a88df781d66d51a02062e0d2";
+
+
+async function GetWeather(){
+    var weatherResult = await (await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=${apiKey}`)).json();
+
+    
+    console.log(weatherResult);
+
+}
+
+
+
+
+addInput.addEventListener('click', GetWeather);
